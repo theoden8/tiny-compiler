b00b0715732f5a5b052866fdc658c72074c0a0c9
Kirill Kolesnikov <theoden8@gmail.com>
2015-08-21 19:37:22 +0300

    VirtualMachine arythmetic operations completed.

--- StackMachine.py
+++ StackMachine.py
@@ -2,17 +2,13 @@
 
 import sys
 import string
+import operator
 
 
 class VM:
 	def push(self):
 		self.stack.append(self.progue[self.line + 1])
 		self.line += 2
-
-	def add(self):
-		self.stack[-2] += self.stack[-1]
-		self.stack.pop()
-		self.line += 1
 
 	def jump(self):
 		self.line = self.progue[self.line + 1]
@@ -29,26 +25,55 @@ class VM:
 		else:
 			self.line = self.progue[self.line + 1]
 
-	def eq(self):
-		self.stack.append(self.stack.pop() == self.stack.pop())
-		self.line += 1
+	def unary_op(func):
+		def opn(self):
+			self.stack.append(func(self.stack.pop()))
+			self.line += 1
+		return opn
 
-	def ne(self):
-		self.stack.append(self.stack.pop() != self.stack.pop())
-		self.line += 1
+	def binary_op(func):
+		def opn(self):
+			self.stack.append(func(self.stack.pop(), self.stack.pop()))
+			self.line += 1
+		return opn
 
 	def exit(self):
 		print self.stack
 		exit(0)
 
-	PUSH, ADD, JUMP, JUMPIF, JUMPUNL, EXIT = range(6)
+	PUSH, JUMP, JUMPIF, JUMPUNL, EXIT, NOT, NEG, POS, ABS, LT, LE, EQ, NE, GE, GT, GT, OR, AND, XOR, LSHIFT, RSHIFT, MOD, ADD, SUB, MUL, DIV, POW = range(27)
 	MATCH = {
 		PUSH    : push,
-		ADD     : add,
 		JUMP    : jump,
 		JUMPIF  : jumpif,
 		JUMPUNL : jumpunless,
 		EXIT    : exit,
+		# UNARY
+		NOT     : unary_op(operator.__not__),
+		NEG     : unary_op(operator.__neg__),
+		POS     : unary_op(operator.__pos__),
+		ABS     : unary_op(operator.__abs__),
+		# BINARY
+		LT      : binary_op(operator.__lt__),
+		LE      : binary_op(operator.__le__),
+		EQ      : binary_op(operator.__eq__),
+		NE      : binary_op(operator.__ne__),
+		GE      : binary_op(operator.__ge__),
+		GT      : binary_op(operator.__gt__),
+		GT      : binary_op(operator.__gt__),
+
+		OR      : binary_op(operator.__or__),
+		AND     : binary_op(operator.__and__),
+		XOR     : binary_op(operator.__xor__),
+
+		LSHIFT  : binary_op(operator.__lshift__),
+		RSHIFT  : binary_op(operator.__rshift__),
+		MOD     : binary_op(operator.__mod__),
+		ADD     : binary_op(operator.__add__),
+		SUB     : binary_op(operator.__sub__),
+		MUL     : binary_op(operator.__mul__),
+		DIV     : binary_op(operator.__div__),
+		POW     : binary_op(operator.__pow__),
 	}
 
 	def __init__(self):
@@ -61,6 +86,10 @@ class VM:
 		self.progue = progue
 		while True:
 			operation = self.progue[self.line]
+			print
+			print self.stack
+			print self.progue
+			print self.line
 			VM.MATCH[operation](self)
 
 
@@ -72,7 +101,11 @@ vm = VM()
 
 #p = [VM.PUSH, 2, VM.PUSH, 2, VM.ADD, VM.EXIT]
 p = [
-	VM.PUSH
-	VM.PUSH
+	VM.PUSH,
+	2,
+	VM.PUSH,
+	2,
+	VM.ADD,
+	VM.EXIT,
 ]
 vm.Perform(p)
